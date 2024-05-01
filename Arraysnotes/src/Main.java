13e92c61e23069ce82a07beebab665838325bedf
Omkar Patil <123920255+TORQUEshot93@users.noreply.github.com>
2024-05-01 22:29:07 +0530

    Delete Arraysnotes directory

--- Arraysnotes/src/Main.java
+++
@@ -1,8 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-         String[] arr = new String[5];
-         System.out.println(arr[0]);
-        }
-    }
